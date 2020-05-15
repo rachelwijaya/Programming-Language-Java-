9b2da4a21d34860dc6d87510290425925054e908
rachelwijaya <55416508+rachelwijaya@users.noreply.github.com>
2020-05-15 18:31:23 +0700

    Create Vehicle.java

---
+++ Interfaces/Vehicle.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+public interface Vehicle {
+    void setName(String name);
+    void setMaxPassenger(int n);
+    void setMaxSpeed(double n);
+    String getName();
+    int getMaxPassenger();
+    double getMaxSpeed();
+
+}
